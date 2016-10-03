3518290846d7a898bddfc0ee1e0d09bdd8683921
Andre Zunino <neyzunino@gmail.com>
2016-10-02 21:23:42 -0300

    Addition of a second box, bouncing ability, SDL loop, ESC to quit, SPACEBAR to apply force to box_1. SDL_RenderPresent was being called more than once per iteration, as the call was in the drawing functions.

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,9 @@ namespace {
     constexpr float WORLD_WIDTH = 10.0f;
     constexpr float WORLD_HEIGHT = 6.667f;
     constexpr float WIDTH_RATIO = WINDOW_WIDTH / WORLD_WIDTH;
-    constexpr float HEIGHT_RATIO = WINDOW_HEIGHT / WORLD_HEIGHT; 
+    constexpr float HEIGHT_RATIO = WINDOW_HEIGHT / WORLD_HEIGHT;
+    constexpr int B2D_SKIN_WIDTH = 3;
+    constexpr int B2D_SKIN_HEIGHT = 3; 
 }
 
 struct Size {
@@ -82,6 +84,7 @@ struct Box {
         fixture_def.shape = &shape;
         fixture_def.density = 1.0f;
         fixture_def.friction = 0.3f;
+        fixture_def.restitution = .5f; // make'em bounce
 
         body->CreateFixture(&fixture_def);
     }
@@ -91,6 +94,9 @@ struct Box {
     Position position() {
         b2Vec2 b2_pos = body->GetPosition();
         return Position {b2_pos.x, b2_pos.y};
+    }
+    b2Body* b2_body() {
+        return body;
     }
     Color color() {
         return color_;
@@ -104,8 +110,8 @@ private:
 
 SDL_Rect make_sdl_rect(const Size& size, const Position& position) {
     SDL_Rect rect;
-    rect.w = size.width * WIDTH_RATIO;
-    rect.h = size.height * HEIGHT_RATIO;
+    rect.w = size.width * WIDTH_RATIO + B2D_SKIN_WIDTH;
+    rect.h = size.height * HEIGHT_RATIO + B2D_SKIN_HEIGHT;
     rect.x = static_cast<int>((position.x - size.width / 2) * WIDTH_RATIO);
     rect.y = WINDOW_HEIGHT - static_cast<int>((position.y + size.height / 2) * HEIGHT_RATIO);
     return rect;
@@ -113,46 +119,77 @@ SDL_Rect make_sdl_rect(const Size& size, const Position& position) {
 
 void draw_ground(Ground& ground, sdl::Window& window) {
     SDL_Rect rect = make_sdl_rect(ground.size(), ground.position());
-    std::clog << "Ground rect: @" << rect.x << ", " << rect.y << "  " << rect.w << "x" << rect.h << '\n';
+    //std::clog << "Ground rect: @" << rect.x << ", " << rect.y << "  " << rect.w << "x" << rect.h << '\n';
     const Color& color = ground.color();
     SDL_SetRenderDrawColor(window.renderer(), color.red, color.green, color.blue, color.alpha);
     SDL_RenderFillRect(window.renderer(), &rect);
-    SDL_RenderPresent(window.renderer());
 }
 
 void draw_box(Box& box, sdl::Window& window) {
     SDL_Rect rect = make_sdl_rect(box.size(), box.position());
-    std::clog << "Box rect: @" << rect.x << ", " << rect.y << "  " << rect.w << "x" << rect.h << '\n';
+    //std::clog << "Box rect: @" << rect.x << ", " << rect.y << "  " << rect.w << "x" << rect.h << '\n';
     const Color& color = box.color();
     SDL_SetRenderDrawColor(window.renderer(), color.red, color.green, color.blue, color.alpha);
     SDL_RenderFillRect(window.renderer(), &rect);
-    SDL_RenderPresent(window.renderer());
+}
+
+void bump_up(Box& box) {
+    b2Body* body = box.b2_body();
+    body->ApplyForce(b2Vec2(0.0f, 15.0f), body->GetPosition(), false);
 }
 
 int main() {
     sdl::SDL sdl(SDL_INIT_VIDEO);
     sdl::Window window("Bars v1.0", WINDOW_WIDTH, WINDOW_HEIGHT);
+    sdl::Clock clock(60);
 
     // creating a world
-    b2Vec2 gravity(0.0f, -10.0f);
+    b2Vec2 gravity(0.0f, -9.8f);
     b2World world(gravity);
 
     Ground ground(world, Size {10.0f, .3f}, Position {5.0f, .15f}, Color {0, 255, 0, 255});
-    Box box_1(world, Size {.2f, .2f}, Position {5.0f, 5.0f}, Color {255, 255, 255, 255});
+    Box box_1(world, Size {.2f, .2f}, Position {4.7f, 5.0f}, Color {255, 0, 0, 255});
+    Box box_2(world, Size {.2f, .2f}, Position {5.3f, 5.0f}, Color {0, 0, 255, 255});
 
     float time_step = 1.0f / 60.0f;
     int32 velocity_iterations = 6;
     int32 position_iterations = 2;
-    for (int i = 0; i < 250; ++i) {
+
+    bool quit = false;
+    SDL_Event event;
+    while (!quit) {
+        clock.startFrame();
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            } else if (event.type == SDL_KEYDOWN) {
+                int keyCode = event.key.keysym.sym;
+                switch (keyCode) {
+                    case SDLK_SPACE:
+                        bump_up(box_1);
+                        break;
+                    case SDLK_ESCAPE:
+                        quit = true;
+                        break;
+                }
+            }
+        }
+        if (quit) break;
+
+        // simulate physics
+        world.Step(time_step, velocity_iterations, position_iterations);
+
+        // draw frame
         window.clear();
         draw_ground(ground, window);
         draw_box(box_1, window);
+        draw_box(box_2, window);
 
-        world.Step(time_step, velocity_iterations, position_iterations);
-        //b2Vec2 position = box.position();
-        //float32 angle = ground_body->GetAngle();
-        //std::cout << std::setprecision(4) << std::setw(10) << position.x << ", " << std::setprecision(4) << position.y << '\n';
-
-        SDL_Delay(10);
+        // render frame
+        SDL_RenderPresent(window.renderer());
+        
+        // control frame time
+        SDL_Delay(clock.remainingFrameTime());
+        clock.endFrame();
     }
 }
